5c0e7e6325e586f9530a8d9083785df32596948b
paulopassos0788 <paulo.passos1@gmail.com>
2025-01-28 13:38:09 -0300

    implementada a entidade autor

---
+++ src/main/java/br/com/passos/demo_spring_rev_jpa/entity/Autor.java
@@ -0,0 +1,58 @@
+package br.com.passos.demo_spring_rev_jpa.entity;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "autores")
+public class Autor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_autor", nullable = false)
+    private Long id;
+
+    @Column(name = "nome", length = 45, nullable = false)
+    private String nome;
+
+    @Column(name = "sobrenome", length = 45, nullable = false)
+    private String sobrenome;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Autor autor = (Autor) o;
+        return Objects.equals(id, autor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
