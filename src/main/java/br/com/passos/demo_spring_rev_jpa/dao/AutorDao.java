607042ddbdb80cce251b7d59fe2998d7adcc6f59
paulopassos0788 <paulo.passos1@gmail.com>
2025-01-28 19:49:24 -0300

    configurando sistema de transacao e inserindo autor no banco de dados

--- src/main/java/br/com/passos/demo_spring_rev_jpa/dao/AutorDao.java
+++ src/main/java/br/com/passos/demo_spring_rev_jpa/dao/AutorDao.java
@@ -4,6 +4,7 @@ import br.com.passos.demo_spring_rev_jpa.entity.Autor;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class AutorDao {
@@ -11,6 +12,7 @@ public class AutorDao {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Transactional(readOnly = false)
     public void salvar(Autor autor) {
         this.entityManager.persist(autor);
     }
