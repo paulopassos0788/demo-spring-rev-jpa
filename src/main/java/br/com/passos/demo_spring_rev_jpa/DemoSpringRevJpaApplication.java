91c7e34ec46ace2570615f266c55f55249fe461e
paulopassos0788 <paulo.passos1@gmail.com>
2025-01-28 13:10:03 -0300

    create init

---
+++ src/main/java/br/com/passos/demo_spring_rev_jpa/DemoSpringRevJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.passos.demo_spring_rev_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringRevJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringRevJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/passos/demo_spring_rev_jpa/DemoSpringRevJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.passos.demo_spring_rev_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringRevJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
